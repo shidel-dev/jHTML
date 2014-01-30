4e59eba97431c65a3692a26987a548d8f0c6c544
shidel.dev <joeshidel@gmail.com>
2014-01-30 12:26:13 -0600

    json stored, and list view rendered

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,7 +1,15 @@
 get '/list' do
+	@list = Post.all
 	erb :list
 end
 
 get '/new' do
 	erb :cms
 end
+
+post'/add' do
+	Post.create(title: params["title"], content: params.to_s)
+	content_type :json
+  Post.last.content.to_json
+end
+
