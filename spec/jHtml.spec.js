6fb435dc424e686aa6b074689299a4c4ecad00fa
shidel.dev <joeshidel@gmail.com>
2014-03-22 10:22:36 -0500

    spec testing for constants

--- spec/jHtml.spec.js
+++ spec/jHtml.spec.js
@@ -1,5 +1,8 @@
 describe("jHtml", function(){
 	it("extends dollar namespace", function(){
-		expect($.jHtml).toBeDefined();
+		expect($.jHtml.parse).toBeDefined();
+	});
+	it("adds constants under jHtml namespace", function(){
+		expect($.jHtml.CONST).toBeDefined();
 	})
 })
