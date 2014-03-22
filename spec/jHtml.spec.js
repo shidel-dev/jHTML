3fda0ea16a2665a3822f59f9cf93de3fa50328e6
shidel.dev <joeshidel@gmail.com>
2014-03-22 11:58:12 -0500

    fetch test suite added

--- spec/jHtml.spec.js
+++ spec/jHtml.spec.js
@@ -2,17 +2,23 @@ describe("jHtml parse", function(){
 	it("extends dollar namespace", function(){
 		expect($.jHtml.parse).toBeDefined();
 	});
+});
+
+descript("jHtml fetch", function(){
+	it("is defined under jHtml namespace", function(){
+		expect($.jhtml.fetch).toBeDefined();
+	})
 })
 
 
 describe("jHtml validate", function(){
 	it("is defined under jHTml namespace", function(){
 		expect($.jHtml.validate).toBeDefined();
-	})
-})
+	});
+});
 
 describe("jHtml CONST", function(){
 	it("adds constants under jHtml namespace", function(){
 		expect($.jHtml.CONST).toBeDefined();
-	})
-})	
+	});
+});	
