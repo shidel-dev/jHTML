d6582acb0d4ee650642257779cc0b457ebb8145c
shidel.dev <joeshidel@gmail.com>
2014-03-22 10:25:16 -0500

    CONST in its own suite

--- spec/jHtml.spec.js
+++ spec/jHtml.spec.js
@@ -1,8 +1,10 @@
-describe("jHtml", function(){
+describe("jHtml parse", function(){
 	it("extends dollar namespace", function(){
 		expect($.jHtml.parse).toBeDefined();
 	});
+})
+decribe("jHtml CONST", function(){
 	it("adds constants under jHtml namespace", function(){
 		expect($.jHtml.CONST).toBeDefined();
 	})
-})
+})	
