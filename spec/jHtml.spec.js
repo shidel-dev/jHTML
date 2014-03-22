d396bc689d64b68f6d24f7af42d82a9c2b38b217
shidel.dev <joeshidel@gmail.com>
2014-03-22 12:07:37 -0500

    spec for basic parsing

--- spec/jHtml.spec.js
+++ spec/jHtml.spec.js
@@ -2,6 +2,16 @@ describe("jHtml parse", function(){
 	it("extends dollar namespace", function(){
 		expect($.jHtml.parse).toBeDefined();
 	});
+
+	it("generates correct markup for a hello world h1", function(){
+		 var header = {
+				"h1" : {
+					"class" : "greeting",
+					"content" : "Hello World"
+				}
+			};
+		expect($.jHtml.parse(header)).toBe("<h1 class='greeting' >Hello World</h1>")
+	})
 });
 
 describe("jHtml fetch", function(){
@@ -9,7 +19,6 @@ describe("jHtml fetch", function(){
 		expect($.jHtml.fetch).toBeDefined();
 	})
 })
-
 
 describe("jHtml validate", function(){
 	it("is defined under jHTml namespace", function(){
