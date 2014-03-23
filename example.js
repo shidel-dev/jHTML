860af2f33e769daa4f345500f0c5c675af421da6
shidel.dev <joeshidel@gmail.com>
2014-03-23 09:56:55 -0500

    working example

--- example.js
+++ example.js
@@ -3,6 +3,10 @@ $(function(){
 		$.jHtml.fetch("./res/json/"+e.target.id + ".json",render);
 		function render(res){
 			$("#container").html(res)
+			$("#submit").click(function(e){
+				e.preventDefault()
+				$(this).parent("form").find("input").val()
+			})
 		}
 	})
 })
